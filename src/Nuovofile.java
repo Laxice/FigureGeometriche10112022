fc9f8d42f83a0cfc59165de9167846d232c1119d
Iryna <irinka25041990@gmail.com>
2022-11-10 12:30:48 +0100

    aggiunto metodo toString

--- src/Nuovofile.java
+++ src/Nuovofile.java
@@ -1,5 +1,14 @@
 public class Nuovofile {
     int Lato = 5;
     double var=123;
-    
+  
+    @Override
+    public String toString() {
+        return "Nuovofile [Lato=" + Lato + "]";
+    }
 }
+
+
+  
+
+
