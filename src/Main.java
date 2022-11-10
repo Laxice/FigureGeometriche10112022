5dafd22eabeab1059a290b6a874051e19d6d75a8
Svitlana <spring5@list.ru>
2022-11-10 12:05:39 +0100

    variabile di prova nel main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("area: " + f1.area() + " perimentro: " + f1.perimetro());
         Forma f2 = new Rettangolo(10, 5);
         System.out.println("area: " + f2.area() + " perimentro: " + f2.perimetro());
+        int variabileProva;//prova modifiche file diversi
 
     }
 }
