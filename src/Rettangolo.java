9a8e57806b4044780dcfcffd348b52ac0c29b18b
Iryna <irinka25041990@gmail.com>
2022-11-10 12:12:06 +0100

    Aggiunto ovverride method

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -33,5 +33,35 @@ public class Rettangolo extends Forma {
     public void setAltezza(double altezza) {
         this.altezza = altezza;
     }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
     
 }
