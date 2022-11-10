272adcac8a0ba0ab20dcb8e35ac0c89adc8288db
Svitlana <spring5@list.ru>
2022-11-10 12:22:22 +0100

    first commit

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -17,5 +17,55 @@ public class Rettangolo extends Forma {
     public double perimetro() {
         return 2*(altezza*lunghezza);
     }
+
+    public double getLunghezza() {
+        return lunghezza;
+    }
+
+    public void setLunghezza(double lunghezza) {
+        this.lunghezza = lunghezza;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+<<<<<<< HEAD
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+=======
+   
+>>>>>>> d59c1994fad1210c7d830b90fee100ae1826517e
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
     
 }
