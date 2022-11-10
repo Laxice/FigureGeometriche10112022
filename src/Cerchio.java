817d84762dcbf52a0cc69722828146253bf41ee3
Iryna <irinka25041990@gmail.com>
2022-11-10 12:05:32 +0100

    Agginto  Override

--- src/Cerchio.java
+++ src/Cerchio.java
@@ -29,6 +29,46 @@ public class Cerchio extends Forma {
         this.raggio = raggio;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(raggio);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cerchio other = (Cerchio) obj;
+        if (Double.doubleToLongBits(raggio) != Double.doubleToLongBits(other.raggio))
+            return false;
+        return true;
+    }
+
+    private double extracted() {
+        return 2*raggio*Math.PI;
+    }
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+    
+}
+
     private double extracted() {
         System.out.println("Hello World"); //Aggiunto un print
         return 2*raggio*Math.PI;
